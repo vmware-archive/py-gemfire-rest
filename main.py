76610093fd0f874d92f35849c4cf196c1d4d677a
madhurak25 <madhurak25@gmail.com>
2014-06-27 18:30:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ client = GemfireClient(hostname, port)
 data = requests.get(client.base_url).json()
 
 
-client.getAllRegionNames()
-client.listAllQueries()
 myRegion = client.getRegion("orders")
+value = {"change":"change"}
+myRegion.put(9,value)
+myRegion.getAll()
+
 myRegion.keys()
 myRegion.get("2")
+
+
+
 
 
 
