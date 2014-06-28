30968e225191ad1cf9033eb7b5cb161e2f4b0bd1
madhurak25 <madhurak25@gmail.com>
2014-06-27 18:21:27 -0700

    Update GemfireClient.py

--- GemfireClient.py
+++ GemfireClient.py
@@ -3,6 +3,7 @@ import json
 
 
 class GemfireClient:
+    
 
     def createURL(self, param):
         self.url = self.base_url + param
@@ -10,13 +11,18 @@ class GemfireClient:
     def __init__(self, hostname, port):
         self.hostname = hostname
         self.port = port
-        self.base_url= "http://" + hostname + ":" + str(port) + "/gemfire-api/v1/"
+        self.base_url = "http://" + hostname + ":" + str(port) + "/gemfire-api/v1/"
 
     def getAllRegionNames(self):
         data = requests.get(self.base_url).json()
         rnames = data['regions']
         names = [region['name'] for region in rnames]
         print names
+        
+    def listAllQueries(self):
+        url = self.base_url + "/queries"
+        data = requests.get(url)
+        print data.text
 
     def getRegion(self,name):
         data = requests.get(self.base_url).json()
@@ -24,43 +30,32 @@ class GemfireClient:
         names = [region['name'] for region in rnames]
         for n in names:
             if n==name:
-                return Region(name,self.base_url)
+                return Region(name, self.base_url)
         else:
             print False
 
 class Region:
 
+   
     def __init__(self, name, base_url):
         self.name = name
         self.base_url = base_url + name
-
-    def getAll(self):
-        data = requests.get(self.base_url)
+        
+    def keys(self):
+        url = self.base_url + "/keys"
+        data = requests.get(url)
         print data.text
-
-    def create(self, key, value):
-        url = self.base_url + "?key=" + str(key)
-        headers = {'content-type': 'application/json'}
-        r = requests.post(url, data=json.dumps(value), headers=headers)
-        if r.status_code == 201 or r.status_code == 202:
-            print True
-        else:
-            print False
-
-    def put(self,key,value):
+        
+    def get(self, key):
         url = self.base_url + "/" + str(key)
-        headers = {'content-type': 'application/json'}
-        r = requests.put(url, data=json.dumps(value), headers=headers)
-        if r.status_code == 200:
-            print True
-        else:
-            print False
-
-
-
-
-
-
+        data = requests.get(url)
+        print data.text
+        
+    #def get(self, ):
+    
 
     
 
+
+        
+        
