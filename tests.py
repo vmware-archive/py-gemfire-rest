a4f70c7a55ba2ec2c1f51290c2caefdc53957052
madhurak25 <madhurak25@gmail.com>
2014-07-07 16:02:15 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,37 +7,37 @@ class SimpleTestCase(unittest.TestCase):
         hostname = "mclaren.gemstone.com"
         port = 8080
         self.client = GemfireClient(hostname, port)
-        self.myRegion = self.client.getRegion("products")
+        self.myRegion = self.client.get_region("products")
         
-    def testgetAllRegionNames(self):
-        allregions = self.client.getAllRegionNames()
+    def test_list_all_regions(self):
+        allregions = self.client.list_all_regions()
         self.assertIsInstance(allregions, list)
         self.assertEqual(allregions[0], 'products')
         self.assertEqual(allregions[1], 'orders')
         self.assertEqual(allregions[2], 'customer')
         
-    def testgetRegion(self):
-        productRegion = self.client.getRegion("products")
+    def testget_region(self):
+        productRegion = self.client.get_region("products")
         self.assertNotEqual(False, productRegion)
         
-    def testnewquery(self):
+    '''def testnewquery(self):
         newquery = self.client.newQuery("cust0018","SELECT * FROM /products WHERE id = 0018")
-        self.assertEqual(newquery, True)
+        self.assertEqual(newquery, True)'''
         
         
-    def testlistAllQueries(self):
-        allqueries = self.client.listAllQueries()
+    def test_list_all_queries(self):
+        allqueries = self.client.list_all_queries()
         #print allqueries
         #self.assertEqual(allqueries, dict)
         
     
-    def testrunquery(self):
-        runquery = self.client.runQuery('SELECT * FROM /orders')
+    def testrun_query(self):
+        runquery = self.client.run_query('SELECT * FROM /orders')
         self.assertIsInstance(runquery, object)
          
     
     def testgetQuery(self):
-        query = self.client.getQuery("cust0013")
+        query = self.client.get_query("cust0013")
         self.assertNotEqual(False, query)
         
     def testcreate(self):
@@ -76,34 +76,37 @@ class SimpleTestCase(unittest.TestCase):
         
     def testdirget(self):
         items = [{"abc":"cdef"},{"123":"8989"}]
-        self.assertEqual(self.myRegion.putAll("108,109",items), True)
+        self.assertEqual(self.myRegion.put("108,109",items), True)
         data1 = self.myRegion[108]
         data2 = self.myRegion[109]
-        print data1, data2
+        self.assertEqual(self.myRegion.delete("108,109"), True)
         
         
         
         
     def testputAll(self):  
         items = [{"abc":"cdef"},{"123":"8989"}]
-        self.assertEqual(self.myRegion.putAll("108,109",items), True)
+        self.assertEqual(self.myRegion.put("108,109",items), True)
         self.assertEqual(self.myRegion.delete("108,109"), True) 
         
         
-    def testupdate(self): 
+    def testupdate(self):
+        items = [{"abc":"cdef"},{"123":"8989"}]
+        self.assertEqual(self.myRegion.put("93",items), True) 
         value_update = {"change":"random"}
         updt = self.myRegion.update(93, value_update)
         self.assertEqual(updt, True)
         #self.assertEqual(self.myRegion.get(93),'{"change":"change12400"}')
         
+        
     def testcompareAndSet(self): 
         comp_value = {"@old":{"change":"random"}, "@new":{"change":"change"}}
-        self.assertEqual(self.myRegion.compareAndSet(93, comp_value), True)
+        self.assertEqual(self.myRegion.compare_and_set(93, comp_value), True)
         
         
     def testgetAll(self):
-        self.assertIsInstance(self.myRegion.getAll(), object)
-        data = self.myRegion.getAll()
+        self.assertIsInstance(self.myRegion.get_all(), object)
+        data = self.myRegion.get_all()
         #print data
     
     def testiterator(self):
@@ -112,7 +115,7 @@ class SimpleTestCase(unittest.TestCase):
           
     def testdelete(self):
         items = {"change":"value_putall"} 
-        self.assertEqual(self.myRegion.putAll("95",items), True)
+        self.assertEqual(self.myRegion.put("95",items), True)
         self.assertEqual(self.myRegion.delete("95"), True)
         keys = self.myRegion.keys()
         #print keys
@@ -123,21 +126,6 @@ class SimpleTestCase(unittest.TestCase):
    
         
     
-        
-   
-        
-    
-        
-   
-        
-    
-        
-   
-        
-    
-        
-        
-
         
 
             
