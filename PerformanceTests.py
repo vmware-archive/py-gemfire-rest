a177af5859c41791e45dca673d4840ae1d01a652
Anirudh1097 <ajain@gopivotal.com>
2014-07-10 16:07:10 -0700

    Create PerformanceTests.py

---
+++ PerformanceTests.py
@@ -0,0 +1,61 @@
+from GemfireClient import *
+import time
+from Customer import *
+
+class PerformanceTests:
+
+    def __init__(self):
+        hostname = raw_input("Enter hostname: ")
+        port = raw_input("Enter port: ")
+        self.client = GemfireClient(hostname,port)
+        self.myRegion = self.client.get_region(raw_input("Enter Region Name: "))
+
+    def warmup(self):
+        for x in range(0,10):
+            self.myRegion.put(x,{"random":"random"})
+            self.myRegion.get(x)
+        self.myRegion.clear()
+
+    def put(self, num):
+        self.warmup()
+        start = time.clock()
+        for x in range(0,num):
+            self.myRegion.put(x,Customer("John Doe", 42))
+        end = time.clock()
+        return (end-start)
+
+    def put_all(self, num):
+        self.warmup()
+        item = {}
+        for x in range(0,num):
+            item[x] = Customer("New Person", 1)
+        start = time.clock()
+        self.myRegion.put_all(item)
+        end = time.clock()
+        return (end-start)
+
+    def get(self, num):
+        self.warmup()
+        for x in range(0,num):
+            self.myRegion.put(x,Customer("John Doe", 42))
+        start = time.clock()
+        for x in range(0,num):
+            self.myRegion.get(x)
+        end = time.clock()
+        return (end-start)
+
+    def run_test(self,testname):
+        filename = raw_input("Enter filename to store run data: ")
+        file = open(filename, "w")
+        op_num = input("Number of operations per run: ")
+        runs = input("Number of runs: ")
+        name = getattr(PerformanceTests,testname)
+        total = 0
+        for x in range(0,runs):
+            y=name(self,op_num)
+            file.write(str(y)+"\n")
+            total+=y
+        file.close()
+        print "The average run time for " + str(op_num) + " " + testname + "s was " + str(total/runs) + " seconds"
+
+
