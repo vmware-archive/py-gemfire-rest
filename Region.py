a32dadd5dfe203f4ec77ae0f182353ca94f1c33f
Anirudh1097 <ajain@gopivotal.com>
2014-07-15 17:29:40 -0700

    Update Region.py

--- Region.py
+++ Region.py
@@ -1,5 +1,6 @@
 import requests
 import jsonpickle
+import logging
 
 
 class Region:
@@ -12,9 +13,14 @@ class Region:
     # Returns all the data in a Region
     def get_all(self):
         url = self.base_url + "?ALL"
-        data = requests.get(url) 
+        data = requests.get(url)
+        logging.debug("Sending request to " + url)
         fdata = jsonpickle.decode(data.text)
-        return fdata[self.name]
+        if data.status_code == 200:
+            logging.debug("Response from server: " + " ," .join(data))
+            return fdata[self.name]
+        else:
+            self.error_response(data)
 
     # Creates a new data value in the Region if the key is absent
     def create(self, key, value):
@@ -22,11 +28,12 @@ class Region:
         headers = {'content-type': 'application/json'}
         jvalue = jsonpickle.encode(value)
         data = requests.post(url, data=jvalue, headers=headers)
+        logging.debug("Sending request to " + url)
         if data.status_code == 201:
+            logging.debug("The value " + str(value) + " was created in the region for the key " + str(key))
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Updates or inserts data for a specified key
     def put(self, key, value):
@@ -34,39 +41,47 @@ class Region:
         headers = {'content-type': 'application/json'}
         jvalue = jsonpickle.encode(value)
         data = requests.put(url, data=jvalue, headers=headers)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug("The value " + str(value) + " was put in the region for the key " + str(key))
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Returns all keys in the Region
     def keys(self):
         url = self.base_url + "/keys"
         data = requests.get(url)
+        logging.debug("Sending request to " + url)
         fdata = jsonpickle.decode(data.text)
-        return fdata["keys"]
+        if data.status_code == 200:
+            logging.debug("Response from server: " + " ," .join(data))
+            return fdata["keys"]
+        else:
+            self.error_response(data)
 
     # Returns the data value for a specified key
     def get(self, *arg):
         sub_url = ','.join(str(key) for key in arg)
         url = self.base_url + "/" + sub_url + "?ignoreMissingKey=true"
         data = requests.get(url)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug("Response from server: " + " ," .join(data))
             return jsonpickle.decode(data.text)
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Method to support region[key] notion
     def __getitem__(self, key):
         url = self.base_url + "/" + str(key) + "?ignoreMissingKey=true"
         data = requests.get(url)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug("Response from server: " + " ," .join(data))
             return jsonpickle.decode(data.text)
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Insert or updates data for a multiple keys specified by a hashtable
     def put_all(self, item):
@@ -75,11 +90,12 @@ class Region:
         headers = {'content-type': 'application/json'}
         jvalue = jsonpickle.encode(item.values())
         data = requests.put(url, data=jvalue, headers=headers)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug(str(item) + " was put into the region")
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Updates the data in a region only if the specified key is present
     def update(self, key, value):
@@ -87,11 +103,12 @@ class Region:
         headers = {'content-type': 'application/json'}
         jvalue = jsonpickle.encode(value)
         data = requests.put(url, data=jvalue, headers=headers)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug("The value at key: " + str(key) + " was updated to " + str(value))
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Compares old values and if identical replaces with a new value
     def compare_and_set(self, key, oldvalue, newvalue):
@@ -100,28 +117,40 @@ class Region:
         value = {"@old":oldvalue,"@new":newvalue}
         jvalue = jsonpickle.encode(value)
         data = requests.put(url, data=jvalue, headers=headers)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug(str(oldvalue) + " was replaced with " + str(newvalue) + " at the key " + str(key))
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Deletes the corresponding data value for the specified key
     def delete(self, *arg):
         sub_url = ','.join(str(key) for key in arg)
         url = self.base_url + "/" + sub_url
         data = requests.delete(url)
+        logging.debug("Sending request to " + url)
         if data.status_code == 200:
+            logging.debug("The values for the keys: " + str(arg) + " were deleted from the region")
             return True
         else:
-            print str(data.status_code) + ": " + data.reason
-            return False
+            self.error_response(data)
 
     # Deletes all data in the Region
     def clear(self):
         data = requests.delete(self.base_url)
         if data.status_code == 200:
+            logging.debug("All data was cleared from the region")
             return True
         else:
+            self.error_response(data)
+
+    # Processes HTTP error responses
+    def error_response(self,data):
+        if data!=400 or data!=409 or data!=405:
+            logging.warning("Response from server: " + str(data.status_code) + " " + data.reason + " - " + data.text)
             print str(data.status_code) + ": " + data.reason
+            return False
+        else:
+            logging.debug("Response from server: " + str(data.status_code) + " " + data.reason + " - " + data.text)
             return False
