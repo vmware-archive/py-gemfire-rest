9b9fb7c22846ed45f707ccfb5386bef720bbd7bb
Anirudh1097 <ajain@gopivotal.com>
2014-07-18 17:13:58 -0700

    Update Repository.py

--- Repository.py
+++ Repository.py
@@ -4,7 +4,7 @@ from Region import *
 class Repository:
 
     # Initializes a Repository Object
-    def __init__(self,name, base_url,type):
+    def __init__(self, name, base_url, type):
         self.name = name
         self.base_url = base_url + name
         self.type = type
@@ -12,22 +12,22 @@ class Repository:
 
     # Deletes the specified Object from the region
     def delete(self, entities):
-        if isinstance(entities,list):
-            temp = ' , '.join(str(key.id)for key in entities)
+        if isinstance(entities, list):
+            temp = ' , '.join(str(key.id) for key in entities)
             return self.region.delete(temp)
-        if isinstance(entities,int):
+        if isinstance(entities, int):
             return self.region.delete(entities)
         else:
             return self.region.delete(entities.id)
-    
+
     # Deletes all data in the region
     def delete_all(self):
         return self.region.clear()
-        
+
     # Checks whether an Object with the given id exists
     def exists(self, id):
-        boolean = self.region.get(id)
-        if boolean == None:
+        value = self.region.get(id)
+        if value is None:
             return False
         else:
             return True
@@ -35,29 +35,25 @@ class Repository:
     # Returns all data in the region
     def find_all(self):
         return self.region.get_all()
-    
+
     # Retrieves Object(s) by the given ID(s) from the region
     def find(self, ids):
-        if isinstance(ids,list):
-            temp = ",".join(str(key)for key in ids)
+        if isinstance(ids, list):
+            temp = ",".join(str(key) for key in ids)
             return self.region.get(temp)
         else:
             return self.region.get(ids)
 
     # Saves(s) all given Objects in the region
     def save(self, entities):
-        if isinstance(entities,list):
+        if isinstance(entities, list):
             item = {}
             for entity in entities:
                 item[entity.id] = entity
             return self.region.put_all(item)
         else:
-            return self.region.put(entities.id,entities)
+            return self.region.put(entities.id, entities)
 
     # Instantiates and returns a Region object
     def get_region(self):
         return Region(self.name, self.base_url, self.type)
-
-
-
-    
